41063dd9b17521cbdccfdc2cf58affff0afc2237
ViniciusZanetti <58536337+ViniciusZanetti@users.noreply.github.com>
2021-12-15 17:00:42 -0300

    .

--- alura-studies/src/components/Cronometro/index.tsx
+++ alura-studies/src/components/Cronometro/index.tsx
@@ -1,17 +1,32 @@
 import Botao from "../Botão";
 import Relogio from "./Relogio";
-import style from './Cronometro.module.scss';
+import style from "./Cronometro.module.scss";
+import { ITarefa } from "../../types/tarefa";
+import { useEffect, useState } from "react";
+import tempoParaSegundos from "../../common/utils/Date";
 
-export default function Cronometro() {
+interface Props {
+  selecionado: ITarefa | undefined;
+}
+
+export default function Cronometro({ selecionado }: Props) {
+  const [tempo, setTempo] = useState<number>(
+    tempoParaSegundos(String(selecionado?.tempo))
+  );
+
+  useEffect(() => {
+    if(selecionado?.tempo){
+      setTempo(tempoParaSegundos(selecionado.tempo));
+    }
+  },[selecionado])
   return (
     <div className={style.cronometro}>
       <p className={style.titulo}>Escolha um card e inicie o Cronômetro</p>
+      tempo: {tempo}
       <div className={style.relogioWrapper}>
         <Relogio />
       </div>
-      <Botao>
-        Começar!
-      </Botao>
+      <Botao>Começar!</Botao>
     </div>
-  )
+  );
 }
